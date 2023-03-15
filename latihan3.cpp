fc1fd00310362166be9285649e9751a046d00cf7
rafiazharr17 <rafipilo17@gmail.com>
2023-03-15 15:46:52 +0700

    Membuat program dengan kondisi if else

---
+++ latihan3.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+using namespace std;
+
+double rerata(double a, double b){
+    return (a + b)/2;
+}
+
+string status(double r){
+    if (r >= 60){
+        return "lulus";
+    }else {
+        return "gagal";
+    }
+}
+
+string status2(double r, double n){
+    if (r >= 60 && n >= 70){
+        return "lulus";
+    }else {
+        return "gagal";
+    }
+}
+
+string status3(double r, double n){
+    if (r >= 60 || n >= 70){
+        return "lulus";
+    }else {
+        return "gagal";
+    }
+}
+
+int main(){
+    double nilaiMath, nilaiBindo;
+
+
+    cout << "Nilai Matematika = ";
+    cin >> nilaiMath;
+    cout << "Nilai Bahasa Indonesia = ";
+    cin >> nilaiBindo;
+
+    //rerata = (nilaiMath + nilaiBindo)/2;
+
+    //rata = rerata(nilaiMath + nilaiBindo)
+    //st = status(rerata(nilaiMath + nilaiBindo));
+
+    // if (rata >= 60){
+    //      status = "lulus";
+    // }else {
+    //      status = "gagal"
+    // }
+
+    cout << "Nilai rata-rata nya = " << rerata(nilaiMath, nilaiBindo);
+    cout << "\nStatus Kelulusan = " << status(rerata(nilaiMath, nilaiBindo));
+    cout << "\nStatus Kelulusan = " << status2(rerata(nilaiMath, nilaiBindo), nilaiMath);
+    cout << "\nStatus Kelulusan = " << status3(rerata(nilaiMath, nilaiBindo), nilaiMath);
+}
